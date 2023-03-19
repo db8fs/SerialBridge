6a7507c4c44e75196e4bc337d3f02e3734ea0a4a
falk.schilling@gmail.com <falk.schilling@gmail.com>
2023-03-19 12:45:11 +0100

    Fix Wifi for RPi0W in Buildroot 2023.02

--- buildroot-2023.02/board/raspberrypi/post-build.sh
+++ buildroot-2023.02/board/raspberrypi/post-build.sh
@@ -10,10 +10,13 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
-#cp package/busybox/S10mdev ${TARGET_DIR}/etc/init.d/S10mdev
-#chmod 755 ${TARGET_DIR}/etc/init.d/S10mdev
-#cp package/busybox/mdev.conf ${TARGET_DIR}/etc/mdev.conf
+cp package/busybox/S10mdev ${TARGET_DIR}/etc/init.d/S10mdev
+chmod 755 ${TARGET_DIR}/etc/init.d/S10mdev
+cp package/busybox/mdev.conf ${TARGET_DIR}/etc/mdev.conf
 
 cp board/raspberrypi/interfaces ${TARGET_DIR}/etc/network/interfaces
 cp board/raspberrypi/wpa_supplicant.conf ${TARGET_DIR}/etc/network/wpa_supplicant.conf
 #cp board/raspberrypi/sshd_config ${TARGET_DIR}/etc/sshd_config
+
+# fix wrong symbolic link for kmod in buildroot 2023.02
+ln -sf /usr/bin/kmod ${TARGET_DIR}/bin/kmod
