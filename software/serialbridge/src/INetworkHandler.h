01b3b71b81961ee44f432394ff9a5d59dd4acdd5
Falk Schilling <Falk.Schilling.de@ieee.org>
2023-10-30 17:17:16 +0100

    Fix build

--- software/serialbridge/src/INetworkHandler.h
+++ software/serialbridge/src/INetworkHandler.h
@@ -1,7 +1,7 @@
 #ifndef INETWORKHANDLER_H_
 #define INETWORKHANDLER_H_
 
-#include <cstdint>
+#include <cstddef>
 
 /** implementers may get notified about network events */
 class INetworkHandler
@@ -9,7 +9,7 @@ class INetworkHandler
 public:
     virtual ~INetworkHandler() {}
 
-    virtual void onNetworkReadComplete(const char* msg, size_t length) = 0;
+    virtual void onNetworkReadComplete(const char* msg, std::size_t length) = 0;
     virtual void onNetworkClientAccept() = 0;
     virtual void onNetworkClientDisconnect() = 0;
 };
